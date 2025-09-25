3a3be845a5dd6ed8a54ac662dff0f426ead9fcb9
Santymrk2 <69694846+santymrk2@users.noreply.github.com>
2025-09-25 20:22:29 -0300

    Allow wide sheet layouts for boletin report

--- frontend-ecep/src/components/ui/sheet.tsx
+++ frontend-ecep/src/components/ui/sheet.tsx
@@ -38,13 +38,49 @@ const sheetVariants = cva(
         top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
         bottom:
           'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
-        left: 'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
+        left: 'inset-y-0 left-0 h-full border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left',
         right:
-          'inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
+          'inset-y-0 right-0 h-full border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right',
+      },
+      size: {
+        default: '',
+        sm: '',
+        lg: '',
+        xl: '',
+        full: '',
       },
     },
+    compoundVariants: [
+      {
+        side: ['left', 'right'],
+        size: 'default',
+        className: 'w-3/4 sm:max-w-sm',
+      },
+      {
+        side: ['left', 'right'],
+        size: 'sm',
+        className: 'w-full sm:max-w-sm',
+      },
+      {
+        side: ['left', 'right'],
+        size: 'lg',
+        className: 'w-full sm:max-w-xl md:max-w-3xl',
+      },
+      {
+        side: ['left', 'right'],
+        size: 'xl',
+        className:
+          'w-full sm:max-w-3xl md:max-w-5xl lg:max-w-6xl xl:max-w-7xl 2xl:max-w-[90rem]',
+      },
+      {
+        side: ['left', 'right'],
+        size: 'full',
+        className: 'w-full max-w-full',
+      },
+    ],
     defaultVariants: {
       side: 'right',
+      size: 'default',
     },
   }
 );
@@ -56,12 +92,12 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = 'right', className, children, ...props }, ref) => (
+>(({ side = 'right', size, className, children, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
-      className={cn(sheetVariants({ side }), className)}
+      className={cn(sheetVariants({ side, size }), className)}
       {...props}
     >
       {children}
